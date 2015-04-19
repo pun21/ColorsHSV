bd087fac398567c17c0517e9898dee6bdcea715d
pun21 <pun.szelok@ufl.edu>
2015-04-19 14:55:46 -0400

    working but still messy, need to fix bugs for going backward and forward

--- app/src/main/java/com/spun/colorshsv/CustomFragment.java
+++ app/src/main/java/com/spun/colorshsv/CustomFragment.java
@@ -23,23 +23,31 @@ public class CustomFragment extends ListFragment {
     private static int MAX_VALUE = 1;
     private static int MIN_VALUE = 0;
     private ArrayList<float[]> hues;
+    private String mTag;
+    private float h, s, v;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Bundle bundle = this.getArguments();
-        float hsv[][] = new float[13][];
+        float hsv[][] = new float[10][];
         for (int i = 0; i < hsv.length; i++) {
             hsv[i] = bundle.getFloatArray("color "+i);
         }
 
-        ArrayList<float[]> hsvList = new ArrayList<float[]>();
-        //ArrayList<HSV> hsvList = new ArrayList<HSV>();
+        mTag = bundle.getString("tag");
+
+        ArrayList<float[]> hsvList = new ArrayList<>();
+
         for (int i = 0; i < hsv.length; i++) {
-            //HSV color = new HSV(hsv[i]);
             hsvList.add(hsv[i]);
         }
 
+        if (mTag == "Fourth") {
+            h = bundle.getFloat("h");
+            s = bundle.getFloat("s");
+            v = bundle.getFloat("v");
+        }
         setListAdapter(new LevelTwoAdapter(getActivity(), hsvList));
     }
     public CustomFragment() {}
@@ -53,9 +61,24 @@ public class CustomFragment extends ListFragment {
     public void onListItemClick(ListView listView, View v, int position, long id) {
         super.onListItemClick(listView, v, position, id);
 
-        Toast.makeText(getActivity(), "Item has been clicked, -->Lv.2", Toast.LENGTH_SHORT).show();
-        //replace with another fragment
-        ((MainActivity)getActivity()).replaceFragment();
+        int level = 0;
+        if (mTag == "First")
+            level = 2;
+        else if (mTag == "Second")
+            level = 3;
+        else
+            level = 4;
+        if (mTag == "Third") {
+            //replace with another fragment
+            Toast.makeText(getActivity(), "Item has been clicked, -->Lv."+level, Toast.LENGTH_SHORT).show();
+            ((MainActivity)getActivity()).replaceFragment(position);
+        }
+        else if (mTag == "Fourth")
+            Toast.makeText(getActivity(), "Go back to select another color.", Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(getActivity(), "Item has been clicked, -->Lv."+level, Toast.LENGTH_SHORT).show();
+            ((MainActivity)getActivity()).replaceFragment(position);
+        }
     }
 
     public class LevelTwoAdapter extends ArrayAdapter<float[]> {
@@ -66,30 +89,70 @@ public class CustomFragment extends ListFragment {
         }
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            float[] hsv = getItem(position);
-            int colorLeft = Color.HSVToColor(hsv);
-            int colorRight;
-
-            if (position < size-1)
-                colorRight = Color.HSVToColor(getItem(position+1));
-            else {
-                float[] color = getItem(position);
-                color[0] = color[0]-30;
-                colorRight = Color.HSVToColor(color);
-            }
-
-
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
             }
 
             TextView textView = (TextView) convertView.findViewById(R.id.text);
-            //textView.setBackgroundColor(color);
+            float[] hsv = getItem(position);
+            int colorLeft = Color.WHITE;
+            int colorRight = Color.WHITE;
 
-            GradientDrawable drawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[] {colorLeft, colorRight});
-            textView.setBackground(drawable);
+            if (mTag == "First") {
+                //vary the hue, constant 100% saturation, constant 100%value*/
+
+                colorLeft = Color.HSVToColor(hsv);
+                if (position < size - 1)
+                    colorRight = Color.HSVToColor(getItem(position + 1));
+                else {
+                    float[] color = getItem(position);
+                    color[0] = color[0] + 30;
+                    colorRight = Color.HSVToColor(color);
+                }
+                GradientDrawable drawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{colorLeft, colorRight});
+                textView.setBackground(drawable);
+            }
+            else if (mTag == "Second") {
+                /*keep the hue range the same, vary the saturation, constant 100%value*/
+                colorLeft = Color.HSVToColor(hsv);
+                if (position < size - 1)
+                    colorRight = Color.HSVToColor(getItem(position + 1));
+                else {
+                    float[] color = getItem(position);
+                    color[0] = color[0] - 30;
+                    color[1] = color[1] - MAX_SATURATION/size;
+                    colorRight = Color.HSVToColor(color);
+                }
+                GradientDrawable drawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{colorLeft, colorRight});
+                textView.setBackground(drawable);
+            }
+            else if (mTag == "Third") {
+                /*keep the hue range and the saturation range the same, vary the value*/
+                colorLeft = Color.HSVToColor(hsv);
+                if (position < size - 1)
+                    colorRight = Color.HSVToColor(getItem(position + 1));
+                else {
+                    float[] color = getItem(position);
+                    color[0] = color[0] - 30;
+                    color[2] = color[2] - MAX_VALUE/size;
+                    colorRight = Color.HSVToColor(color);
+                }
+                GradientDrawable drawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{colorLeft, colorRight});
+                textView.setBackground(drawable);
+            }
+            else {
+                //todo try to use the empty in a listview
+                if (position == 0) {
+                    textView.setText("HSV values selected:\n" +
+                                     "Hue Range: " + h + "-" + (h+30) + "\n" +
+                                     "Saturation: " + s + "\n" +
+                                     "Value: " + v);
+                    textView.setTextSize(18);
+                }
+            }
+
+
             return convertView;
         }
     }
-
 }
--- app/src/main/java/com/spun/colorshsv/MainActivity.java
+++ app/src/main/java/com/spun/colorshsv/MainActivity.java
@@ -12,72 +12,93 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
     private static int MAX_HUE = 360;
     private static int MIN_HUE = 0;
-    private static int MAX_SATURATION = 1;
-    private static int MIN_SATURATION = 0;
-    private static int MAX_VALUE = 1;
-    private static int MIN_VALUE = 0;
+    private static float MAX_SATURATION = 1;
+    private static float MIN_SATURATION = 0;
+    private static float MAX_VALUE = 1;
+    private static float MIN_VALUE = 0;
     private static int mTag = 0;
 
-    private static ArrayList<Integer> rainbowList;
     private float[][] hsv;
     private String[] mFragmentTag = new String[]{"First", "Second", "Third", "Fourth"};
+    private float h;
+    private float s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        int rainbow[] = this.getResources().getIntArray(R.array.rainbow);
-
-        hsv = new float[13][];
+        hsv = new float[10][];
         for (int i = 0; i < hsv.length; i++) {
-            float[] temp = new float[]{360 - (i * 30), MAX_SATURATION, MAX_VALUE};
+            float[] temp = new float[]{0 + (i * 30), MAX_SATURATION, MAX_VALUE};
             hsv[i] = temp;
         }
-        rainbowList = new ArrayList<>();
+
         ArrayList<float[]> hsvList = new ArrayList<>();
 
         for (int i = 0; i < hsv.length; i++) {
-
             hsvList.add(hsv[i]);
         }
 
-        for (int i = 0; i < rainbow.length; i++) {
-            rainbowList.add(rainbow[i]);
-        }
-
         CustomFragment huesFragment = new CustomFragment();
         Bundle bundle = new Bundle();
         for (int i = 0; i < hsv.length; i++) {
             bundle.putFloatArray("color "+i, hsv[i]);
         }
+        bundle.putString("tag", mFragmentTag[0]);
         huesFragment.setArguments(bundle);
+
+         /*The first fragment contains gradients of pure spectral hues with 100% saturation
+        /*100% value.
+        */
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(android.R.id.content, huesFragment, mFragmentTag[mTag]);
+        ft.addToBackStack(null);
         ft.commit();
         mTag++;
-        /*The first fragment contains gradients of pure spectral hues with 100% saturation
-        /*100% value.
-        */
+    }
 
-
-
-
+    public void replaceFragment(int position) {
         /*The second fragment displays the same hues at varying saturation levels (100% to 0%)
         /*at a constant 100% value.
          */
-    }
-
-    public void replaceFragment() {
         CustomFragment huesFragment = new CustomFragment();
         Bundle bundle = new Bundle();
+        if (mFragmentTag[mTag] == "Second") {
+            for (int i = 0; i < hsv.length; i++) {
+                float sat = MAX_SATURATION - i*(MAX_SATURATION/(float)hsv.length);
+                float[] temp = new float[]{0 + (position * 30), MAX_SATURATION - i*(MAX_SATURATION/hsv.length), MAX_VALUE};
+                hsv[i] = temp;
+            }
+            h = position;
+        }
+        else if (mFragmentTag[mTag] == "Third") {
+            for (int i = 0; i < hsv.length; i++) {
+                float[] temp = new float[]{0 + (h * 30), MAX_SATURATION - position*(MAX_SATURATION/hsv.length), MAX_VALUE-i*(MAX_VALUE
+                /hsv.length)};
+                hsv[i] = temp;
+            }
+            s = position;
+        }
+        else {
+            for (int i = 0; i < hsv.length; i++) {
+                float[] temp = new float[]{0, MAX_SATURATION, MAX_VALUE};
+                hsv[i] = temp;
+            }
+            bundle.putFloat("h", 0 + (h * 30));
+            bundle.putFloat("s", MAX_SATURATION - s*(MAX_SATURATION/hsv.length));
+            bundle.putFloat("v", MAX_VALUE-position*(MAX_VALUE
+                    /hsv.length));
+        }
         for (int i = 0; i < hsv.length; i++) {
             bundle.putFloatArray("color "+i, hsv[i]);
         }
+        bundle.putString("tag", mFragmentTag[mTag]);
         huesFragment.setArguments(bundle);
         FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.replace(android.R.id.content, huesFragment, mFragmentTag[mTag]);
+        ft.addToBackStack(null);
         ft.commit();
         Toast.makeText(this, mFragmentTag[mTag++] + " fragment replaced the first.", Toast.LENGTH_SHORT).show();
     }
